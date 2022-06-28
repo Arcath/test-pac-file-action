f8c2b1394295463d796e6bb6f88d0b5b9e027277
Arcath <adam@arcath.net>
2022-06-28 21:28:34 +0100

    add url to test and additional test

--- dist/index.js
+++ dist/index.js
@@ -11323,11 +11323,18 @@ async function run() {
   try {
     const file = core.getInput('file')
     const url = core.getInput('url')
+    const expectedResult = core.getInput('result')
     core.info(`Testing ${file}`)
 
     const result = testPacFile({file, url})
 
     core.setOutput('result', result)
+
+    if (expectedResult !== result) {
+      throw new Error(
+        `PAC File test failed, got "${result}", expected "${expectedResult}"`
+      )
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
--- index.test.js
+++ index.test.js
@@ -2,10 +2,17 @@ const testPacFile = require('./lib/test-pac-file')
 const path = require('path')
 
 test('it should take a file and return a result', async () => {
-  const result = await testPacFile({
+  let result = await testPacFile({
     file: `file://${path.join(__dirname, 'samples', 'direct.pac')}`,
     url: 'https://www.google.com'
   })
 
   expect(result).toBe('DIRECT')
+
+  result = await testPacFile({
+    file: 'file://./samples/proxy-google.pac',
+    url: 'https://www.google.com'
+  })
+
+  expect(result).toBe('PROXY proxy.example.com:8080')
 })
